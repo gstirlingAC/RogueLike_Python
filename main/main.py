c3b695d64f14d41e5fdcf37084faf938089f954e
gstirlingAC <gordon.stirling@ayrshire.ac.uk>
2017-06-16 17:35:25 +0100

    Tutorial 3 -  Create the main game loop In this tutorial we begin to flesh out a structure for our code, specifically the main game loop.  This time we will replace the pseudocode with actual python code.  We will define the basic game loop and give the player a way to quit out of the program.  We will also create new file (module) which will hold all of our constant variables (variables whose values never change).
    In the next tutorial we will draw something to the newly created surface.

--- main/main.py
+++ main/main.py
@@ -4,12 +4,17 @@ Author: Gordon Stirling
 Python/Pygame project
 http://www1.ayrshire.ac.uk
 
-Tutorial 2 -  Pseudocode for main game loop
+Tutorial 3 -  Create the main game loop
 In this tutorial we begin to flesh out a structure for our code, 
-specifically the main game loop.  
+specifically the main game loop.  This time we will replace the
+pseudocode with actual python code.  We will define the basic
+game loop and give the player a way to quit out of the program. 
 
-We begin that process by creating a 'blueprint' for our game 
-loop through a process called pseudo-programming (or pseudocode).
+We will also create new file (module) which will hold all of our 
+constant variables (variables whose values never change).
+
+In the next tutorial we will draw something to the newly created
+surface.
 
 """
 
@@ -18,34 +23,43 @@ import pygame
 import libtcodpy as libtcod
 
 # game modules
-
-
-'''Pseudo-programming (pseudocode):
-Pseudo-code is an informal way to express the design of a 
-computer program or an algorithm.  The aim is to get the idea quickly 
-and also easy to read without details.  It is like a young child 
-putting sentences together without any grammar.  There are several ways 
-of writing pseudo-code; there are no strict rules.  But to reduce ambiguity 
-between what you are required to do and what you express let’s base the 
-pseudo code on the few defined conventions'''
+import settings
 
 def game_main_loop():
     '''This function loops the main game'''
 
-    #TODO create a variable to control when the game ends, set it to False
+    game_quit = False
 
-    #TODO game while loop:
-        #TODO get player input
-        #TODO process input
+    while not game_quit:
+        # get player input
+        events = pygame.event.get()
+
+        #process input
+        for event in events:
+            if event.type == pygame.QUIT:
+                game_quit = True
+
         #TODO draw the game
 
-    #TODO quit the game
+    # quit the game
+    pygame.quit()
+    exit()
+
 
 def game_init():
     '''This function initialises the main window and pygame'''
 
-    #TODO make a global (available to all modules) variable to hold the game window (surface)
-    #TODO initialise pygame
-    #TODO create window (surface)
+    # make a global (available to all modules) variable to hold the game window (surface)
+    global SURFACE_MAIN
 
-#TODO call the functions to make the game run
+    # initialise pygame
+    pygame.init()
+
+    #create window (surface)
+    SURFACE_MAIN = pygame.display.set_mode((settings.SURFACE_WIDTH, settings.SURFACE_HEIGHT))
+
+
+# call the functions to make the game run
+if __name__ == '__main__':
+    game_init()
+    game_main_loop()

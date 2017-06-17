ae7494e7eb3412ed268396194664f4b3ecd45beb
gstirlingAC <gordon.stirling@ayrshire.ac.uk>
2017-06-17 14:56:09 +0100

    Tutorial 7 -  Player movement In this tutorial we are going to create code to allow the player to effect the position of the player sprite.  We are going to create an Actor (an actor is basically anything that can interact with anything else in the world) object and create code to allow the system to draw it and to allow the player to move its' position.

--- main/main.py
+++ main/main.py
@@ -7,13 +7,12 @@ http://www1.ayrshire.ac.uk
 Sprites created by: DawnBringer
 https://opengameart.org/content/dawnlike-16x16-universal-rogue-like-tileset-v181
 
-Tutorial 6 -  Tiles and Maps
-In this tutorial we are going to create individual tiles which we will then use to
-construct a map to form the game world.  We will begin by defining a 'struct' type 
-object (Python doesn't technically support Structs, but any C-based language does.
-Python does support Classes (Classes and Structs are very similar) however so we 
-will use a Class in order to create our struct-type tile object.  We will then
-define a function which will let us generate an empty map.
+Tutorial 7 -  Player movement
+In this tutorial we are going to create code to allow the player to effect the 
+position of the player sprite.  We are going to create an Actor (an actor is
+basically anything that can interact with anything else in the world) object
+and create code to allow the system to draw it and to allow the player to move
+its' position.
 
 """
 
@@ -30,6 +29,21 @@ class struct_Tile:
     def __init__(self, block_path):
         self.block_path = block_path
 
+# object definitions
+class obj_Actor:
+    def __init__(self, x, y, sprite):
+        self.x = x # map address
+        self.y = y # map address
+        self.sprite = sprite
+
+    def draw(self):
+        SURFACE_MAIN.blit(self.sprite, (self.x * settings.CELL_WIDTH, self.y * settings.CELL_HEIGHT))
+
+    def move(self, dx, dy):
+        if GAME_MAP[self.x + dx][self.y + dy].block_path == False:
+            self.x += dx
+            self.y += dy
+
 # map definition
 def create_map():
     new_map = [[ struct_Tile(False) for y in range(0, settings.MAP_HEIGHT) ] for x in range(0, settings.MAP_WIDTH)]
@@ -38,6 +52,7 @@ def create_map():
     new_map[10][15].block_path = True
 
     return new_map
+
 
 def draw_map(map_to_draw):
 
@@ -51,6 +66,7 @@ def draw_map(map_to_draw):
                 # draw floor
                 SURFACE_MAIN.blit(settings.S_FLOOR, (x * settings.CELL_WIDTH, y * settings.CELL_HEIGHT))
 
+
 # function definitions
 def draw_game():
     '''We will use this function to draw objects to the surface'''
@@ -62,16 +78,17 @@ def draw_game():
     draw_map(GAME_MAP)
 
     # draw the character
-    SURFACE_MAIN.blit(settings.S_PLAYER, (settings.PLAYER_POS_DEFAULT))
+    PLAYER.draw()
 
     # update the display
     pygame.display.flip()
+
 
 def game_init():
     '''This function initialises the main window and pygame'''
 
     # make a global (available to all modules) variable to hold the game window (surface)
-    global SURFACE_MAIN, GAME_MAP
+    global SURFACE_MAIN, GAME_MAP, PLAYER
 
     # initialise pygame
     pygame.init()
@@ -80,6 +97,8 @@ def game_init():
     SURFACE_MAIN = pygame.display.set_mode((settings.SURFACE_WIDTH, settings.SURFACE_HEIGHT))
 
     GAME_MAP = create_map()
+    PLAYER = obj_Actor(0, 0, settings.S_PLAYER)
+
 
 def game_main_loop():
     '''This function loops the main game'''
@@ -90,10 +109,20 @@ def game_main_loop():
         # get player input
         events = pygame.event.get()
 
-        # TODO process input - more events to come
+        #  process input - more events to come
         for event in events:
             if event.type == pygame.QUIT:
                 game_quit = True
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    PLAYER.move(0, -1)
+                if event.key == pygame.K_DOWN:
+                    PLAYER.move(0, 1)
+                if event.key == pygame.K_LEFT:
+                    PLAYER.move(-1, 0)
+                if event.key == pygame.K_RIGHT:
+                    PLAYER.move(1, 0)
 
         # draw the game
         draw_game()
